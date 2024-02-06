6c87018f5fe889f3f0c3d39400029140ce8dbc9a
zephyrus <joelogira@gmail.com>
2024-02-06 21:11:23 +0300

    Created thank you component

---
+++ app/thank-you/page.tsx
@@ -0,0 +1,9 @@
+export default function ThankYou() {
+    return (
+        <main className="min-h-[100svh] grid items-center justify-center p-5">
+            <h1 className="text-3xl">
+                Thank You for your feedback
+            </h1>
+        </main>
+    )
+}
